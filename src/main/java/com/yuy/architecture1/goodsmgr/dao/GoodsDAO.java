8e1a6693b1300a623414bbeb061c4d48eaf3b0ff
yuyivie <yuyivie@163.com>
2016-07-21 16:23:05 +0800

    商品前端模块

---
+++ src/main/java/com/yuy/architecture1/goodsmgr/dao/GoodsDAO.java
@@ -0,0 +1,11 @@
+package com.yuy.architecture1.goodsmgr.dao;
+
+import org.springframework.stereotype.Repository;
+import com.yuy.architecture1.common.dao.BaseDAO;
+
+import com.yuy.architecture1.goodsmgr.vo.GoodsQueryModel;
+
+@Repository
+public interface GoodsDAO extends BaseDAO<GoodsModel,GoodsQueryModel>{
+	
+}
